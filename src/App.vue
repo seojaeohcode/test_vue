b9af56ebfec4562aed76f4134aa7e898de8118a4
seojaeoh <seojaeohcode@gmail.com>
2022-07-08 11:45:02 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <script>
-import TextBind from './components/TextBind.vue'
 import Textbind from './components/Textbind.vue'
 export default {
-  components : {
-    TextBind: TextBind,
-    Textbind
-}
+  components: {
+    components: {
+      Textbind,
+    },
+  },
+  data() {
+      return {
+        bind_url : 'http://www.daum.net',
+        bind_a : {
+          bind_url : 'http://www.daum.net',
+          domain_name : 'daum',
+        }, 
+      }
+    }
 }
 </script>
 
 <template>
-<!-- <Textbind /> -->
+  <img src="https://placeimg.com/100/100/any" alt="image size 100-100">
+  <h2 id="title">Hello World</h2>
+  <a v-bind:href="bind_a.bind_url">{{bind_a.domain_name}}</a>
+  <!-- <Textbind /> -->
 </template>
 
-<style>
+<style scoped>
+#title {
+  color: blueviolet;
+  text-align: center;
+}
 </style>
--- src/components/TextBind.vue
+++ src/components/TextBind.vue
@@ -3,36 +3,36 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 console.log(`New App!`);
 export default {
-  data(){
+  data() {
     return {
-      name : 'SeoJaeOh',
-      user : {
-        name : 'A',
-        age : 30,
+      name: 'SeoJaeOh',
+      user: {
+        name: 'A',
+        age: 30,
       },
-      fruits : ['banana', 'apple', 'melon'],
-      button_html : '<button>v-html</button>',
-      button_hack : `<button onclick='document.body.style="background:blue;"'>css hack</button>`
+      fruits: ['banana', 'apple', 'melon'],
+      button_html: '<button>v-html</button>',
+      button_hack: `<button onclick='document.body.style="background:blue;"'>css hack</button>`
     }
-  }  
+  }
 }
 </script>
 
 <template>
-<div v-html="button_hack"></div>
-<div v-html="button_html"></div>
-<div v-text="name"></div>
-<hr>
-  <div>{{name}}</div>
-  <div>{{user}}</div>
-  <div>{{user.name+" "+user.age}}</div>
-  <div>{{user.name}}:{{user.age}}</div>
-  <div>{{fruits}}</div>
-  <div>{{fruits[2]}}</div>
+  <div v-html="button_hack"></div>
+  <div v-html="button_html"></div>
+  <div v-text="name"></div>
+  <hr>
+  <div>{{ name }}</div>
+  <div>{{ user }}</div>
+  <div>{{ user.name + " " + user.age }}</div>
+  <div>{{ user.name }}:{{ user.age }}</div>
+  <div>{{ fruits }}</div>
+  <div>{{ fruits[2] }}</div>
 </template>
 
 <style scoped>
-div{
+div {
   margin: 1rem;
   border: 0.2rem solid gray;
 }
